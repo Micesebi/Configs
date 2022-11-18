22c0b050c17a551ec60d06e1a4359ed2d5594011
Micesebi <118563234+Micesebi@users.noreply.github.com>
2022-11-18 20:15:00 +0100

    Add files via upload
    my Qtile configs

---
+++ qtile/modules/screens.py
@@ -0,0 +1,89 @@
+
+from libqtile import bar
+from .widgets import *
+from libqtile.config import Screen
+from modules.keys import terminal
+import os
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [   widget.Sep(padding=3, linewidth=0, background="#ff7faa"),
+                widget.Sep(padding=4, linewidth=0, background="#faff74"), 
+                widget.GroupBox(
+                                highlight_method='line',
+                                this_screen_border="#ffffff",
+                                this_current_screen_border="#ff54e7",
+                                active="#f51200",
+                                inactive="#fd14ff",
+                                background="#faff74"),
+                widget.TextBox(
+                       text = '',
+                       padding = 0,
+                       fontsize = 28,
+                       foreground='#faff74'
+                       ),    
+                widget.Prompt(),
+                widget.Spacer(length=5),
+                widget.WindowName(foreground='#ffffff',fmt='{}'),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.CurrentLayoutIcon(scale=0.75),
+                widget.CheckUpdates(
+                    update_interval=1800,
+                    distro="Arch_yay",
+                    display_format="{updates} Updates",
+                    foreground="#ffffff",
+                    mouse_callbacks={
+                        'Button1':
+                        lambda: qtile.cmd_spawn(terminal + ' -e yay -Syu')
+                    },
+                    background="#2f343f"),
+                widget.Systray(icon_size = 20),
+                widget.TextBox(
+                       text = '',
+                       padding = 0,
+                       fontsize = 28,
+                       foreground='#faff74'
+                       ), 
+                volume,
+                widget.TextBox(                                                                    
+                       text = '',
+                       padding = 0,
+                       fontsize = 28,
+                       foreground='#faff74',
+                       ),   
+                widget.TextBox(
+                       text = '',
+                       padding = 0,
+                       fontsize = 28,
+                       foreground='#faff74'
+                       ),    
+                widget.Clock(format=' %Y-%m-%d %a %I:%M %p',
+                             background="#faff74",
+                             foreground='#2E035B'),
+                                                widget.TextBox(                                                
+                                                
+                       text = '',
+                       padding = 0,
+                       fontsize = 28,
+                       foreground='#faff74',
+                       ),   
+                widget.TextBox(
+                    text='',
+                    mouse_callbacks= {
+                        'Button1':
+                        lambda: qtile.cmd_spawn(os.path.expanduser('~/.config/rofi/powermenu.sh'))
+                    },
+                    foreground='#000000'
+                )
+                
+            ],
+            30,  # height in px
+            background="#ff7faa"  # background color
+        ), ),
+]
