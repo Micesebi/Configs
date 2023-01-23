9bce00ce6e4d7bd10bee8360fa7090684ca3085d
Micesebi <118563234+Micesebi@users.noreply.github.com>
2023-01-23 16:38:10 +0000

    Add files via upload
    Laptop update with added Battery indicator, WiFi and updated systray

--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -1,4 +1,3 @@
-
 from libqtile import bar
 from .widgets import *
 from libqtile.config import Screen
@@ -9,11 +8,11 @@ screens = [
     Screen(
         top=bar.Bar(
             [   widget.Sep(padding=3, linewidth=0, background="#ff7faa"),
-                widget.Sep(padding=4, linewidth=0, background="#faff74"), 
+                widget.Sep(padding=4, linewidth=0, background="#ff7faa"), 
                 widget.GroupBox(
                                 highlight_method='line',
                                 this_screen_border="#ffffff",
-                                this_current_screen_border="#ff54e7",
+                                this_current_screen_border="#ff54ef",
                                 active="#f51200",
                                 inactive="#fd14ff",
                                 background="#faff74"),
@@ -32,54 +31,93 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.CurrentLayoutIcon(scale=0.75),
-                widget.CheckUpdates(
-                    update_interval=1800,
-                    distro="Arch_yay",
-                    display_format="{updates} Updates",
-                    foreground="#ffffff",
-                    mouse_callbacks={
-                        'Button1':
-                        lambda: qtile.cmd_spawn(terminal + ' -e yay -Syu')
-                    },
-                    background="#2f343f"),
-                widget.Systray(icon_size = 20),
+		widget.TextBox(
+                       text = '',
+                       padding = 0,
+                       fontsize = 28,
+                       foreground='#ff00bb',
+                       ), 
+                widget.CurrentLayoutIcon(
+			scale=0.75,
+			background='#ff00bb',
+			),
+                widget.Systray(
+		       icon_size = 20,
+                       foreground='#faff74',
+                       background='#ff00bb',
+		       ),			
                 widget.TextBox(
                        text = '',
                        padding = 0,
                        fontsize = 28,
-                       foreground='#faff74'
-                       ), 
+                       foreground='#faff74',
+                       background='#ff00bb',
+			), 
                 volume,
                 widget.TextBox(                                                                    
                        text = '',
                        padding = 0,
                        fontsize = 28,
                        foreground='#faff74',
+		       Background='#ff99bb',
                        ),   
                 widget.TextBox(
                        text = '',
                        padding = 0,
                        fontsize = 28,
-                       foreground='#faff74'
+                       foreground='#faff74',
+		       Background='#ff99bb',
                        ),    
                 widget.Clock(format=' %Y-%m-%d %a %I:%M %p',
                              background="#faff74",
-                             foreground='#2E035B'),
-                                                widget.TextBox(                                                
-                                                
+                             foreground='#2e035b'),
+                
+		widget.TextBox(                                                                                                
                        text = '',
                        padding = 0,
                        fontsize = 28,
                        foreground='#faff74',
+		       Background='#ff99bb',
                        ),   
-                widget.TextBox(
+		
+		widget.TextBox(
+                       text = '',
+                       padding = 0,
+                       fontsize = 28,
+                       foreground='#faff74',
+		       Background='#ff99bb',
+                       ),                    
+
+		widget.BatteryIcon(
+			padding = 0,
+			fontsize = 28,
+			background = '#faff74',
+			foreground = '#2e035b',
+			),
+		
+		widget.Battery(
+			fontsize = 13,
+			padding = 0,
+			background = '#faff74',
+			foreground = '#000000',
+			update_interval=15,
+			),
+
+		widget.TextBox(
+			text = '',
+			padding = 0,
+			fontsize = 28,
+			foreground = '#faff74',
+		        Background='#ff99bb',	
+			),
+		widget.TextBox(
                     text='',
                     mouse_callbacks= {
                         'Button1':
                         lambda: qtile.cmd_spawn(os.path.expanduser('~/.config/rofi/powermenu.sh'))
                     },
-                    foreground='#000000'
+                    foreground='#000000',
+		    background='#ff99bb'
                 )
                 
             ],
