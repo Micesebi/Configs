204e3b75236373721e91d22f4d919da8a9fd5138
Micesebi <118563234+Micesebi@users.noreply.github.com>
2023-01-23 16:38:33 +0000

    Add files via upload

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,26 +1,13 @@
 #!/bin/sh
-
-function run {
-	if ! pgrep $1 ;
-	then
-		$@&
-	fi
-}
-
-
-
-
-
-
-
-picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed &
-flameshot &
 variety &
+flameshot &
+nm-applet &
+picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # Low battery notifier
-~/.config/qtile/scripts/check_battery.sh & disown &
+~/.config/qtile/scripts/check_battery.sh & disown
 
 # Start welcome
-eos-welcome & disown &
+eos-welcome & disown
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
